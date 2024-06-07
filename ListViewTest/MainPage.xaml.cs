1cc9148d09c765c04a27e444b4262ade3f541fff
Enigma86d <56714594+Enigma86d@users.noreply.github.com>
2024-06-07 10:37:17 +0300

    Add files via upload

---
+++ ListViewTest/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using ListViewTest.models;
+
+namespace ListViewTest
+{
+    public partial class MainPage : ContentPage
+    {
+        private MeasurementCollection data = new MeasurementCollection();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MyListView.ItemsSource = data;
+            data.Add(new Measurement(0));
+            data.Add(new Measurement(1));
+            data.Add(new Measurement(2));
+            data.Add(new Measurement(3));
+            data.Add(new Measurement(4));
+            data.Add(new Measurement(5));
+            data.Add(new Measurement(6));
+            data.Add(new Measurement(7));
+            data.Add(new Measurement(8));
+            data.Add(new Measurement(9));
+        }
+
+    }
+
+}
---
+++ ListViewTest/models/model.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ListViewTest.models
+{
+    public class Measurement
+    {
+        public string Item1 { get; set; }
+        public string Item2 { get; set; }
+        public string Item3 { get; set; }
+        public string Item4 { get; set; }
+
+        public Measurement(int no)
+        {
+            Item1 = no.ToString();
+            Item2 = (no+10).ToString();
+            Item3 = (no+100).ToString();
+            Item4 = (no + 1000).ToString();
+        }
+    }
+
+    public class MeasurementCollection : ObservableCollection<Measurement>
+    {
+        public MeasurementCollection()
+        {
+        }
+    }
+
+}
